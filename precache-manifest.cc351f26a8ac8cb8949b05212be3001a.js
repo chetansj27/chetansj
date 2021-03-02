42db3b70a0a95addd653ce891e9edabe329436ca
chetan <csj5483@gmail.com>
2021-03-02 15:20:33 +0530

    Updates

---
+++ precache-manifest.cc351f26a8ac8cb8949b05212be3001a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b6160d9932dbfe38e51efc87435314",
+    "url": "/chetansj/index.html"
+  },
+  {
+    "revision": "9d72f4c56c3312ff3252",
+    "url": "/chetansj/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "e177392f2e8946ff946e",
+    "url": "/chetansj/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "e48fd1cb3088db922ded",
+    "url": "/chetansj/static/css/main.eb311589.chunk.css"
+  },
+  {
+    "revision": "5fd2ec886678347ee62e",
+    "url": "/chetansj/static/js/2.5409a4d8.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/chetansj/static/js/2.5409a4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d72f4c56c3312ff3252",
+    "url": "/chetansj/static/js/3.379875bf.chunk.js"
+  },
+  {
+    "revision": "e177392f2e8946ff946e",
+    "url": "/chetansj/static/js/4.fde37db6.chunk.js"
+  },
+  {
+    "revision": "e48fd1cb3088db922ded",
+    "url": "/chetansj/static/js/main.38c7dd0e.chunk.js"
+  },
+  {
+    "revision": "165f029a27153abc676b",
+    "url": "/chetansj/static/js/runtime-main.f6498599.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/chetansj/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/chetansj/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/chetansj/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/chetansj/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "f286b49da68debcb3f0c9399d3af09dc",
+    "url": "/chetansj/static/media/donation.f286b49d.jpg"
+  },
+  {
+    "revision": "b3175d3ca736fa0484e1665f5fc7cc46",
+    "url": "/chetansj/static/media/farmer.b3175d3c.jpg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/chetansj/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "1432c64fb2206035ffcbf3554fe1ec2b",
+    "url": "/chetansj/static/media/medicine.1432c64f.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/chetansj/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "091eda3a1bcf0ec5cab6730546bbcec0",
+    "url": "/chetansj/static/media/volumetree.091eda3a.png"
+  }
+]);
